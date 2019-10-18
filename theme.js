8beb62769ed3fa2f6299890669ed147c941aebdc
Jeff Hampton <jhampton@gmail.com>
2019-10-18 17:57:15 -0500

    Initial

---
+++ theme.js
@@ -0,0 +1,12 @@
+import { condensed } from '@mdx-deck/themes'
+
+export const summit = {
+    ...condensed,
+    prism: {
+        graphQL: {
+            fontSize: '12px',   
+        }
+    },
+    code: { fontSize: '12px' },
+    pre: { fontSize: '12px' },
+}
